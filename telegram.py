5a596cd7a05763569d8a34b139615f7d35663833
johtai <53095170+johtai@users.noreply.github.com>
2021-04-09 19:54:53 +0300

    Final commit 1.0

--- telegram.py
+++ telegram.py
@@ -1,8 +1,10 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
 from telegram import ReplyKeyboardMarkup
-from Constants import *
+import requests
 
+
+TOKEN = "c"
 reply_keyboard = [['/generate', '/recognize']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
@@ -12,27 +14,49 @@ def start(update, context):
 
 
 def recognize(update, context):
-    update.message.reply_text("АХАХАХАХАХА ВАХ КАКОЙ СМЕШНОЙ МЕМ Я АЖ ОБКЕКАЛСЯ", reply_markup=markup)
+    '''# update.message.reply_text(requests.get("http://134.209.94.3:5000/recognize/im.jpg"))
+    url = 'http://134.209.94.3:5000/recognize'
+    fp = open('im.jpg', 'rb')
+    files = {'image': fp}
+
+    update.message.reply_text(requests.post(url, files=files))
+    fp.close()'''
+
+    photo_path = update.message.photo[-1].get_file().download()
+    fp = open(photo_path, "rb")
+    files = {"image": fp}
+    resp = requests.post("http://134.209.94.3:5000/recognize", files=files).json()
+    update.message.reply_text(f"Meme: {resp['meme']}\nProbability: {resp['probability']}")
+    fp.close()
+    
 
 
 def generate(update, context):
-    # update.message.reply_photo(photo)
-    update.message.reply_text("мем")
+    photo = requests.get("http://134.209.94.3:5000/generate")
+
+    with open("image.png", "wb") as f:
+        f.write(photo.content)
+
+    update.message.reply_photo(open("image.png", "rb"))
 
 
 def error(update, context):
-    update.message.reply_text("Я понимаю только язык МЕМОВ")
+    update.message.reply_text("I am only undersand the MEME language!")
+
+def helpp(update, context):
+    update.message.reply_text("Send me a message to get the information about your meme!")
 
 def karelia(update, context):
-    update.message.reply_text("Вы нашли пасхалку. Хайль Карелия!")
+    update.message.reply_text("Вы нашли пасхалку. Хайль Карелия! Voiten Kunnia! Слава Победе!")
 
     
 def main():
-    updater = Updater(TELEGRAM_TOKEN, use_context=True)
+    updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("recognize", recognize))
+    dp.add_handler(MessageHandler(Filters.photo, recognize))
     dp.add_handler(CommandHandler("generate", generate))
+    dp.add_handler(CommandHandler("recognize", helpp))
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("karelia", karelia))
     dp.add_handler(MessageHandler(Filters.text, error))
